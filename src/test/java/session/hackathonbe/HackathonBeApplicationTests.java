6395282559592ed5909e7aad466f8f29455f1545
sujin <20241005@sungshin.ac.kr>
2025-07-17 14:35:17 +0900

    :gear:Settings

---
+++ src/test/java/session/hackathonbe/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package session.hackathonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
